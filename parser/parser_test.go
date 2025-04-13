4b4c2be972111273145385e1c676f33dbf4b41a4
jaehyeon-crema <jaehyeon.kim@cre.ma>
2025-04-13 13:35:36 +0900

    add: return statement parser test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -66,6 +66,34 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	return true
 }
 
+func TestReturnStatement(t *testing.T) {
+	input := `
+return 5;
+return 10;
+return 993322;
+`
+	lexer := lexer.New(input)
+	p := New(lexer)
+
+	program := p.ParseProgram()
+	checkParserError(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral() not 'return'. got=%s", returnStmt.TokenLiteral())
+		}
+	}
+}
+
 func checkParserError(t *testing.T, p *Parser) {
 	errors := p.Errors()
 	if len(errors) == 0 {
