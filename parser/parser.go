940893605d4ad5cab66b256c8ba91a077a02a696
jaehyeon-crema <jaehyeon.kim@cre.ma>
2025-04-15 23:57:43 +0900

    improve: append error when prefix funtion is nil

--- parser/parser.go
+++ parser/parser.go
@@ -162,9 +162,15 @@ func (p *Parser) registerInfix(t token.TokenType, fn infixParseFn) {
 	p.infixParseFns[t] = fn
 }
 
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) parseExpression(_ int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 	leftExp := prefix()
